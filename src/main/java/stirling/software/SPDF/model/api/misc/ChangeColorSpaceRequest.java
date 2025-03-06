b13dabb8aa79094c7fbe6159a561beb13f292598
“Hanna <hanna@sennero.se>
2025-03-06 16:47:18 +0100

    Add method to load ICC profile from file
    The method loads an ICC profile from a file. The file is found at: https://www.color.org/registry/Coated_Fogra39L_VIGC_300.xalter. The license gives us the right to use, embed, and share it without restriction, but it may not be altered or sold.

--- src/main/java/stirling/software/SPDF/model/api/misc/ChangeColorSpaceRequest.java
+++ src/main/java/stirling/software/SPDF/model/api/misc/ChangeColorSpaceRequest.java
@@ -1,15 +1,30 @@
 package stirling.software.SPDF.model.api.misc;
 
+import java.awt.color.ICC_ColorSpace;
+import java.awt.color.ICC_Profile;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
 import stirling.software.SPDF.model.api.PDFFile;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ChangeColorSpaceRequest extends PDFFile {
+    private ICC_Profile iccProfile;
+    private ICC_ColorSpace cmykColorSpace;
 
-    // Add schemas here
-    // We need to in someway get an Inputstream (or similar for the request ICC profile)
+    public ChangeColorSpaceRequest() throws IOException {
+        String iccFilePath = "resources/static/Coated_Fogra39L_VIGC_300.icc";
 
+        try (InputStream iccProfileStream = new FileInputStream(iccFilePath)) {
+            this.iccProfile = ICC_Profile.getInstance(iccProfileStream);
+        } catch (FileNotFoundException e) {
+            System.err.println("ICC-profilen kunde inte hittas: " + iccFilePath);
+        }
+        this.cmykColorSpace = new ICC_ColorSpace(iccProfile);
+    }
 }
