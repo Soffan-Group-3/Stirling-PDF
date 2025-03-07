c101a37925320aa74c27464c1ade9c23996755a7
OsGrGreen <ozkargronman@gmail.com>
2025-03-07 09:01:07 +0100

    Finilize functionality of color space change API
    These changes finilize the functionality of the color space change API.
    Requests can now be done from the related tool, and is automatically
    downloaded, and seems to work fine.
    
    Important note is that there currently is only one available ICC
    profile, and that the program does not expect resources from
    `src/main/resources` but instead the root directory, which is not ideal.
    This will be needed to be changed before merging to main.
    
    There are also no tests that check the functionality of the code, for
    example if the color conversion is done properly.

--- src/main/java/stirling/software/SPDF/utils/ChangeColorSpace.java
+++ src/main/java/stirling/software/SPDF/utils/ChangeColorSpace.java
@@ -5,17 +5,15 @@ import java.awt.color.ICC_Profile;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 public class ChangeColorSpace {
 
-    public static BufferedImage transformToICC(PDImageXObject src, InputStream icc_profile)
+    public static BufferedImage transformToICC(PDImageXObject src, ICC_Profile icc_profile)
             throws IOException {
         BufferedImage src_img = src.getImage();
-        ICC_Profile ip = ICC_Profile.getInstance(icc_profile);
-        ICC_ColorSpace ics = new ICC_ColorSpace(ip);
+        ICC_ColorSpace ics = new ICC_ColorSpace(icc_profile);
         ColorConvertOp cco = new ColorConvertOp(ics, null);
         BufferedImage result = cco.filter(src_img, null);
         return result;
